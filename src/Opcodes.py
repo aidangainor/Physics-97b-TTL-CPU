29b52593b16c49293fbbd0587e8773b0e8471168
aidangainor <againor@brandeis.edu>
2017-05-11 03:37:26 -0400

    First try decimal display decoding and documented opcodes

--- src/Opcodes.py
+++ src/Opcodes.py
@@ -1,9 +1,46 @@
 # T register acts as a temporary register for all memory transfers
-# order dictates opcode = eg asm_inst[op_code] = instruction
+# Order dictates opcode = eg asm_inst[op_code] = instruction
+# For example, RESET is opcode 0 in decimal and MOV T,FLAGS is opcode 17 in decimal
+
+
 asm_insts = ["RESET", "HALT", "LOAD_BYTE", "LOAD_IND", "STORE_IND", "ADD", "ADD_WC", "SUB", "AND", "OR", "XOR",
              "MOV A,T", "MOV B,T", "MOV C,T", "MOV I,T", "MOV J,T", "MOV FLAGS,T", "MOV T,FLAGS", "MOV T,J", "MOV T,I", "MOV T,C", "MOV T,B", "MOV T,A",
              "JMP_UN", "JMP_Z", "JMP_NZ", "JMP_C", "JMP_NC", "JMP_N", "JMP_NN", "CALL", "RETURN", "NOP"]
 
+"""
+RESET : reset program counter and memory address register (I and J) and fetch very first instruction in ROM
+
+HALT : freeze CPU by not incrementing program counter and causing feedback register to loop to itself
+
+LOAD_BYTE a_byte : load a_byte immediately to the T register
+
+LOAD_IND : load a byte indirectly to the T register pointed by memory address register (I and J)
+
+STORE_IND : store the value in T register to memory address pointed by I and J
+
+ADD : add values A and B and store in T register ; affects the C, Z, and N flags
+
+ADD_WC : same as ADD except we also input carry bit ; affects the C, Z, and N flags
+
+SUB : subtract A from B and store in T, in other words T = B - A ; affects the C, Z, and N flags
+
+AND : bitwise AND registers A and B, store in T ; affects the C, Z, and N flags
+
+OR : bitwise OR registers A and B, store in T ; affects the C, Z, and N flags
+
+XOR : bitwise XOR registers A and B, store in T ; affects the C, Z, and N flags
+
+MOV reg1, reg2 : copy reg2 to reg1, in other words reg1 = reg2
+
+JMP memory_addr : jump to the instruction located at some 16 bit memory address, JMP_UN is an unconditional jump and JMP_NC is jump if carry bit not set
+                  JMP_N is jump if negative bit is set and JMP_NZ is jump if zero bit is not set, meaning the result of ALU operation was not zero
+
+CALL memory_addr : to be implemented w/ stack pointer
+
+RETURN : to be implemented w/ stack pointer
+
+NOP : do nothing for 5 clock cycles
+"""
 
 # Map assembly mnemonic to integer opcode
 asm_to_opcode = {}
--- src/decimal_decoding/BinaryToDecimalMapper.py
+++ src/decimal_decoding/BinaryToDecimalMapper.py
@@ -8,17 +8,20 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from HexWriter import HexWriter
 
+# I/O pin 0 = A, pin 1 = B, pin 2 = C, pin 3 = D, pin 4 = E, pin 5 = F, pin 6 = G, pin 7 = no connection
+# We are using 3 LTS-312AHR 7 seg displays which are common anode, meaning we light LED outputting 0 volts
 seven_seg_encoding = {
-    0 : "",
-    1 : "",
-    2 : "",
-    3 : "",
-    4 : "",
-    5 : "",
-    6 : "",
-    7 : "",
-    8 : "",
-    9 : ""
+        # gfedcba
+    0 : "11000000",
+    1 : "11111001",
+    2 : "10100100",
+    3 : "10110000",
+    4 : "10011001",
+    5 : "10010010",
+    6 : "10000010",
+    7 : "11111000",
+    8 : "10000000",
+    9 : "10011000"
 }
 
 # We need to generate 3 HEX files for each EEPROM connected in parallel to decode binary input into 7 segment display signals
@@ -26,6 +29,7 @@ prefix = "display" + os.path.sep + "bin_to_dec_"
 file_writer_1s = HexWriter(prefix + "1s_place.hex")
 file_writer_10s = HexWriter(prefix + "10s_place.hex")
 file_writer_100s = HexWriter(prefix + "100s_place.hex")
+all_file_writers = [file_writer_1s, file_writer_10s, file_writer_100s]
 
 for i in range(256):
     ones_place_digit = i%10
@@ -38,4 +42,4 @@ for i in range(256):
 
 # Write all hex files to disk
 for file_writer in all_file_writers:
-    file_writer.commit()
+    file_writer.fill_file()
