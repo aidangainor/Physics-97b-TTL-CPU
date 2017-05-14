7612f530794e92f62b2e75b0a2261a2794f12be5
aidangainor <againor@brandeis.edu>
2017-05-13 21:44:39 -0400

    RAM test program made

---
+++ src/assembler/asm_src/ram_tests.asm
@@ -0,0 +1,64 @@
+load_byte 2d
+mov i,t
+
+load_byte 33d
+mov j,t
+
+load_byte 1d
+store_ind
+inc ij
+
+load_byte 2d
+store_ind
+inc ij
+
+load_byte 4d
+store_ind
+inc ij
+
+load_byte 8d
+store_ind
+inc ij
+
+load_byte 16d
+store_ind
+inc ij
+
+load_byte 32d
+store_ind
+inc ij
+
+load_byte 64d
+store_ind
+inc ij
+
+load_byte 128d
+store_ind
+inc ij
+
+load_byte 2d
+mov i,t
+
+load_ind
+inc ij
+
+load_ind
+inc ij
+
+load_ind
+inc ij
+
+load_ind
+inc ij
+
+load_ind
+inc ij
+
+load_ind
+inc ij
+
+load_ind
+inc ij
+
+load_ind
+inc ij
