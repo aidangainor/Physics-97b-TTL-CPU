d5708ced378462c1776104eccf0a9c6145302818
huxtim <huxtim@brandeis.edu>
2017-05-15 02:20:41 -0400

    attempt 2, added outputs for debugging

--- src/assembler/asm_src/arrayCounter.asm
+++ src/assembler/asm_src/arrayCounter.asm
@@ -7,21 +7,25 @@ mov j,t           ; I = 32
 load_byte 22d      ; T = 22
 store_ind         ; array[0] = 22
 inc ij            ; J = J + 1
+output
 
   ;load second index
 load_byte 40d      ; T = 40
 store_ind         ; array[1] = 40
 inc ij            ; J = J + 1
+output
 
   ;load third index
 load_byte 100d      ; T = 100
 store_ind         ; array[2] = 100
 inc ij            ; J = J + 1
+output
 
   ;load final index
 load_byte 255d      ; T = 255
 store_ind         ; array[3] = 255
 inc ij            ; J = J + 1
+output
 
     ;begin construction of loop using the stack to hold the size of the array
   ;go back to beginning of array
@@ -37,7 +41,8 @@ mov a,t      ;move array[n] to a
 load_byte 255d
 mov b,t      ;move 255 to b
 xor           ;xor, if 255, output will be 0
-jmp_z 53d     ;we have reached the end of the array and thus have finished counting
+output
+jmp_z 60d     ;we have reached the end of the array and thus have finished counting
 
   ;add 1 to count which is held in stack
 pop           ;pops count from stack
@@ -45,9 +50,10 @@ mov a,t       ;moves it to register a
 load_byte 1d  ;loads 1 to t
 mov b,t      ;which is then moved to b
 add           ;and we add them so that the count goes up by 1
+output
 push          ;to which we then push back to the stack
 inc ij        ;and we increment ij in order to scan the next index in the array
-jmp_nz 35d    ;and we go back to the start of checking for 255
+jmp_nz 39d    ;and we go back to the start of checking for 255
 
 
   ;output count
