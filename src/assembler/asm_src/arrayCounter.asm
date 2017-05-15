082c4502c71e46981fec20ea93743b2749ff256d
huxtim <huxtim@brandeis.edu>
2017-05-15 12:48:44 -0400

    swap.asm

--- src/assembler/asm_src/arrayCounter.asm
+++ src/assembler/asm_src/arrayCounter.asm
@@ -3,8 +3,8 @@ call &to_index_zero
 
   ;load arrays with any values from 0-254s
   ;load first index
-load_byte 22d       ; T = 22
-store_ind           ; array[0] = 22
+load_byte 100d       ; T = 100
+store_ind           ; array[0] = 100
 inc ij              ; J = J + 1
 output
 
@@ -15,8 +15,8 @@ inc ij              ; J = J + 1
 output
 
   ;load third index
-load_byte 100d      ; T = 100
-store_ind           ; array[2] = 100
+load_byte 22d      ; T = 22
+store_ind           ; array[2] = 22
 inc ij              ; J = J + 1
 output
 
@@ -38,8 +38,7 @@ mov a,t                 ;move array[n] to a
 load_byte 255d
 mov b,t                 ;move 255 to b
 xor                     ;xor, if 255, output will be 0
-output
-jmp_z &output_show      ;we have reached the end of the array and thus have finished counting
+jmp_z &sort     ;we have reached the end of the array and thus have finished counting
 
   ;add 1 to count which is held in stack
 pop           ;pops count from stack to t
@@ -47,12 +46,36 @@ mov a,t       ;moves it to register a
 load_byte 1d  ;loads 1 to t
 mov b,t      ;which is then moved to b
 add           ;and we add them so that the count goes up by 1
-output
 push          ;to which we then push back to the stack
 inc ij        ;and we increment ij in order to scan the next index in the array
 jmp_nz &count_loop    ;and we go back to the start of checking for 255
 
-  output_show:
+
+sort:
+call &to_index_zero               ;sort starts from zero-th index
+call &decrement_loop_counter      ;we index starting from zero
+push  ;push again to store how many checks will be done this time
+
+internal_loop:
+load_ind
+mov b,t
+inc ij
+load_ind
+mov a,t
+sub
+jmp_n &switch
+
+pop
+move a,t
+load_byte 11111111
+move b,t
+add
+jmp_z &decrease outer loop
+jmp_un 67d
+
+
+
+output_show:
 pop
 output
 halt
@@ -64,4 +87,13 @@ to_index_zero:
   mov j,t           ; I = 32
   load_byte 0d
   mov i,t
+  return
+
+decrement_loop_counter:
+  pop
+  move a,t
+  load_byte 11111111
+  move b,t
+  add
+  push
   return
---
+++ src/assembler/asm_src/swap.asm
@@ -0,0 +1,68 @@
+call &to_index_zero
+
+  ;load first index
+load_byte 100d       ; T = 100
+store_ind           ; array[0] = 100
+inc ij              ; J = J + 1
+output
+
+  ;load second index
+load_byte 40d       ; T = 40
+store_ind           ; array[1] = 40
+inc ij              ; J = J + 1
+output
+
+;load second index
+load_byte 255d       ; T = 255
+store_ind           ; array[255] = 255
+inc ij              ; J = J + 1
+output
+
+call &to_index_zero
+load_ind
+mov b,t
+inc ij
+load_ind
+mov a,t
+call &swap
+call &to_index_zero
+call &show_array_contents
+halt
+
+swap: ;puts A before B in the index
+  mov t,b
+  mov c,t ;c stores b
+  mov t,i
+  mov b,t ;b stores i
+  mov t,a
+  mov i,t ;i stores a
+  load_byte 11111111b
+  mov a,t
+  add ; t = index-1
+  mov a,t ; a = index-1
+  mov t,i ; t = a
+  mov b,t ; b = a
+  mov t,a ; t = index-1
+  mov i,t ; i = index-1
+  mov t,b ; t = a
+  load_ind
+  inc ij
+  mov t,c
+  load_ind
+  return
+
+to_index_zero:
+  load_byte 32d     ; load T register w/ 32,
+  mov j,t           ; I = 32
+  load_byte 0d
+  mov i,t
+  return
+
+show_array_contents:
+  load_ind
+  output
+  inc ij
+  load_byte 255d
+  xor
+  jmp_nz &show_array_contents
+  return
