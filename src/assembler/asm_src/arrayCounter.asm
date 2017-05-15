6aa0a9c413e3895b8983fd26cd07b527a4c06183
huxtim <huxtim@brandeis.edu>
2017-05-15 02:11:41 -0400

    compiled

--- src/assembler/asm_src/arrayCounter.asm
+++ src/assembler/asm_src/arrayCounter.asm
@@ -28,14 +28,14 @@ inc ij            ; J = J + 1
 load_byte 32d     ; load T register w/ 32,
 mov j,t           ; I = 32
 load_byte 0d
-move i,t
+mov i,t
 push ; push 0 to stack
 
   ;check if empty array by xoring the current index and 255 which we state as the last array symbol
 load_ind      ;check index of array, starting at 1
-move a,t      ;move array[n] to a
+mov a,t      ;move array[n] to a
 load_byte 255d
-move b,t      ;move 255 to b
+mov b,t      ;move 255 to b
 xor           ;xor, if 255, output will be 0
 jmp_z 53d     ;we have reached the end of the array and thus have finished counting
 
@@ -43,7 +43,7 @@ jmp_z 53d     ;we have reached the end of the array and thus have finished count
 pop           ;pops count from stack
 mov a,t       ;moves it to register a
 load_byte 1d  ;loads 1 to t
-move b,t      ;which is then moved to b
+mov b,t      ;which is then moved to b
 add           ;and we add them so that the count goes up by 1
 push          ;to which we then push back to the stack
 inc ij        ;and we increment ij in order to scan the next index in the array
@@ -52,4 +52,4 @@ jmp_nz 35d    ;and we go back to the start of checking for 255
 
   ;output count
 pop
-out
+output
